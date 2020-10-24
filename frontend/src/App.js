e2102ef88df5d7f82f14de9a9b0c998d745d62eb
joseiciano <joseiciano@protonmail.com>
2020-10-24 19:33:08 -0400

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 import CustomNavbar from "./components/CustomNavbar";
 import { Container, Nav } from "react-bootstrap";
 import "./style.css";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Button } from "react-bootstrap";
 import Lottie from "react-lottie";
-import animationData from "./images/analytics.json";
+import graphData from "./images/analytics.json";
+import computerData from "./images/sitting_at_computer.json";
 
 function App() {
-  const defaultOptions = {
+  const graphLottieData = {
     loop: true,
     autoplay: true,
-    animationData: animationData,
+    animationData: graphData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const computerLottieData = {
+    loop: true,
+    autoplay: true,
+    animationData: computerData,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
@@ -36,9 +46,30 @@ function App() {
               me there, hanky panky... You can touch, you can play, if you say,
               "I'm always yours."
             </h5>
+            <Button className="my-2 btn btn-blue mx-3">
+              Click Here To Get Access
+            </Button>
           </div>
           <div className="my-3 col-md-6 col-12 d-flex justify-content-center align-items-center">
-            <Lottie options={defaultOptions} height={400} width={400} />
+            <Lottie options={graphLottieData} height={400} width={400} />
+          </div>
+        </Container>
+
+        <Container>
+          <div class="row vh-100 mb-5 top-section-2">
+            <div class="order-md-1 order-2 col-md-6 col-12 d-flex justify-content-center align-items-center">
+              <Lottie options={computerLottieData} height={400} width={400} />
+            </div>
+            <div class="main-content col-md-6 col-12 order-md-2 order-1 d-flex justify-content-center">
+              <h5 class="text-center dark-blue">
+                Another night, another dream but always you It's like a vision
+                of love that seems to be true Another night another dream but
+                always you In the night I dream of love so true Just another
+                night, another vision of love You feel joy, you feel pain, 'cuz
+                nothing will be the same Just another night is all that it takes
+                To understand the difference between lovers and fakes
+              </h5>
+            </div>
           </div>
         </Container>
       </Container>
