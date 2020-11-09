3b4b49cac9a350eb6e5b0b650a741441e1516f66
SlayerPhantom <unravelphantom@gmail.com>
2020-11-09 15:07:59 -0500

    third commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,11 @@ import Homepage from './pages/Homepage';
 import Speakers from './pages/Speakers';
 import Events from './pages/Events';
 import ProtectedRoute from './auth/ProtectedRoute';
-import Landing from './pages/Landing';
 export default function App() {
 	return (
 		<Router>
 			<Switch>
 				<Route path="/" exact component={Homepage} />
-				<Route path="/landing" exact component={Landing} />
 				<ProtectedRoute path="/speakers" exact component={Speakers} />
 				<ProtectedRoute path="/events" exact component={Events} />
 			</Switch>
