6f1d1a64297e5e7727c371ba34a6f3b8c577c5f0
joseiciano <joseiciano@protonmail.com>
2020-12-02 18:15:32 -0500

    removed duplicate routes folder in dir

--- frontend/src/auth/ProtectedRoute.js
+++ frontend/src/auth/ProtectedRoute.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
 
 function ProtectedRoute({ component: Component, ...rest }) {
-	const isAuthenticated = localStorage.getItem('isAuthenticated');
-	return (
-		<Route
-			{...rest}
-			render={(props) => {
-				if (isAuthenticated) {
-					return <Component {...props} />;
-				} else {
-					return (
-						<Redirect
-							to={{
-								pathname: '/',
-								state: {
-									from: props.location,
-								},
-							}}
-						/>
-					);
-				}
-			}}
-		/>
-	);
+  const isAuthenticated = localStorage.getItem("isAuthenticated");
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (isAuthenticated) {
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
 }
 
 export default ProtectedRoute;
