772607a99e997cf7149a5a9482a10123d04221de
SlayerPhantom <68765217+SlayerPhantom@users.noreply.github.com>
2020-12-03 00:59:15 -0500

    Update PageNotFound.js

--- frontend/src/pages/PageNotFound.js
+++ frontend/src/pages/PageNotFound.js
@@ -2,18 +2,10 @@ import React from 'react';
 
 export default function PageNotFound() {
 	return (
-		{/*
+		{
 		<div
-			style={{
-				height: '100vh',
-				width: '100wh',
-				display: 'flex',
-				justifyContent: 'center',
-			}}
 		>
 			<h1>404 Page not found</h1>
 		</div>
-		*/}
-		<h1>Hello world</h1>
 	);
 }
