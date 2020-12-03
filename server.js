e74f98fc3ea1c6ef7c5fb28a8a246955b4193152
SlayerPhantom <68765217+SlayerPhantom@users.noreply.github.com>
2020-12-03 01:13:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path')
 require('dotenv').config({ path: './config/config.env' });
 
 const app = express();
