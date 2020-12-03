53d40cd7942a2a036d37de7c6a2024725d4c2180
SlayerPhantom <68765217+SlayerPhantom@users.noreply.github.com>
2020-12-02 23:42:42 -0500

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
 function isAuthenticated(req, res, next) {
 	if (req.isAuthenticated()) return next();
-	else res.redirect('http:localhost:3000');
+	else res.redirect('https://ubreakifix2020summit.herokuapp.com/');
 }
